ec7af338208e79ba7d4d8c18053d44a2ea6c2bb0
Shaheen Ghiassy <sghiassy@linkedin.com>
2024-08-10 18:33:51 -0400

    Add bat to linux setup

--- scripts/setup_linux.sh
+++ scripts/setup_linux.sh
@@ -22,3 +22,6 @@ echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable
 sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
 sudo apt update
 sudo apt install -y eza
+
+# Install bat
+sudo apt install bat -y
